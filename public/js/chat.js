35d6bdbde5bfd34ea933899bb4a106d4b0771bf6
Teo Gonzalez Calzada CS <tgonzalezc@grupoimperial.net>
2018-07-13 18:40:47 -0600

    Code cleaned

---
+++ public/js/chat.js
@@ -0,0 +1,65 @@
+var socket = io();
+var username = "";
+
+function login() {
+    usr = $("#login-username").val();
+    if (usr == "") {
+        alert("Please specify a username")
+        return;
+    }
+
+    socket.emit('login', usr, function (data) {
+        if (data) {
+            $("#login").fadeOut("slow");
+            username = usr;
+        } else {
+            alert("Username already taken");
+        }
+    });
+
+    $("#login-username").val('');
+}
+
+//Send message
+function sendMessage() {
+    var $msg = $send.val();
+    if($msg == ""){
+        return;
+    }
+    $msg = $msg.replace(/(?:\r\n|\r|\n)/g, '<br>');
+    socket.emit('chat', {
+        msg: $msg,
+        room: $currentroom
+    });
+
+    $send.val('');
+}
+
+//Server sends a new message
+socket.on('chat', function (msg) {
+    //Calculate time
+    var d = new Date();
+    //Create new message
+    var $main = $('<div></div>').addClass("message clearfix " + my);
+    var $message = $("<div></div>").html(msg.msg);
+    var $chip = $("<span></span>").addClass("mdl-chip mdl-chip--contact");
+    var $icon = $("<span></span>").text(msg.user.charAt(0)).addClass("mdl-chip__contact mdl-color--teal mdl-color-text--white");
+    var $nickname = $("<span></span>").text(msg.user).addClass("mdl-chip__text message-nick");
+    var $date = $("<small></small>").text(d.toDateString());
+
+    //Build the message html and append it to the correct room div
+    $nickname.append($date);
+    $chip.append($icon);
+    $chip.append($nickname);
+    $main.append($message);
+    $main.append($chip);
+
+    $('#messages-' + msg.room).append($main);
+
+    // Scroll down
+    $('#messages-' + msg.room).animate({
+        scrollTop: $('#messages-' + msg.room).prop("scrollHeight")
+    }, 1500);
+    // Notificate based on user preferences
+    notificate("New message from " + msg.user + " on " + msg.room, msg.msg);
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,211 +1,99 @@
-function toggle(div){ $(div).animate({width: 'toggle'}) }
+var $currentroom = "main";
+var $send = "";
 
-    $('#upload-btn').on("click", function(){
-        //$('#upload-form').fadeIn('slow');
-        toggle('#upload-form');
+$(document).ready(function () {
+    $send = $("#send-text");
+
+    // Fade loading page on load
+    $('#login').fadeIn('fast', function(){
+        $('#loading').fadeOut('fast');
     });
 
-    $('.close-form').on("click", function(){
-        //$('#' + $(this).attr("for")).fadeOut('slow');
-        toggle('#' + $(this).attr("for"))
-    });
-
-    $('#messages').on("click", '.chat-image', function(){ 
-    	window.open( $(this).attr("src") ) 
-    });
-
-    function sendFile(inp) {
-        if (inp.files && inp.files[0]) {
-            var FR= new FileReader();
-            FR.fileName = inp.files[0].name
-            FR.onload = function(e) {
-                socket.emit('chat image', e.target.result);
-                $('.upload-form').append('<div class="success">The file ' + e.target.fileName + ' was sent to everybody</div>');
-                console.log(e.target);
-                setTimeout( function(){
-                    toggle('#upload-form');
-                    $('.success').remove();
-                }, 1500);
-            };       
-            FR.readAsDataURL( inp.files[0] );
-        }
-    }
-
-    $('#send-image').on("click", function(){
-        sendFile(document.getElementById('input-image'));
-    });
-
-    function getPlainText(html) {
-        return $('<p>' + html + '</p>').text();
-    }
-
-    var socket = io();
-    var ret, gUser;
-
-    $('form').submit(function(){
-    	if($('#m').val() != ''){
-    		socket.emit('chat message', $('#m').val());
-	        $('#m').val('');
-    	}
-    	return false;
-    });
-
-    socket.on('chat message', function(msg, usr){
-        console.log('Mensaje', msg);
-        var c = (usr == gUser) ? "message yourself" : "message";
-        var data = (usr == gUser) ? "" : '<p class="username">' + usr + '</p>';
-    	var newMessage = '<div class="clearfix">' + data + '<li class="' + c + '">';
-    	newMessage += msg;
-    	newMessage += '</li></div>';
-        $('#messages').append( newMessage ); scrollDown();
-        if(usr != gUser){
-            showSmartAlert(usr, getPlainText(msg)); playAudio();
-        }
-        $('.message > p').emoticonize();
-        $('.message > p > a').attr("target", "_blank");
-    });
-
-    socket.on('chat image', function(img, usr){
-        var c = (usr == gUser) ? "message yourself" : "message";
-        var data = (usr == gUser) ? "" : '<p class="username">' + usr + '</p>';
-        var newMessage = '<div class="clearfix">' + data + '<li class="' + c + '">';
-        newMessage += '<img class="chat-image" src="' + img + '" alt="image_on_chat">';
-        newMessage += '</li></div>';
-        $('#messages').append( newMessage ); scrollDown();
-        if(usr != gUser){
-            showSmartAlert(usr, 'Image sent'); playAudio();
+    // Login actions
+    $("#login-username").on("keypress", function(e){
+        if(e.which ==  13){
+            login();         
         }
     });
-
-    socket.on('new user', function(usr, area){
-        $('#messages').append("<div class='clearfix'><li class='newUser message'>A new user has logged to the chat!<br>Hi! <b>" + usr + "</b> from " + area + "</li></div>"); scrollDown();
-        showSmartAlert('New User', 'Say hello to ' + usr);
+    $("#login-btn").on("click", function(){
+        login();
     });
 
-    socket.on('disconnect', function(usr){
-        if(usr == "transport error" || usr == "io server disconnect" || usr == "transport close"){
-            $('#messages').append("<div class='clearfix'><li class='disconnect message'>You has disconnected for Network failure. Please reload the page</li></div>");
-            location.reload();
-        }else{
-            $('#messages').append("<div class='clearfix'><li class='disconnect message'>The user <b>" + usr + "</b> has leaved the room</li></div>"); scrollDown();
-            showSmartAlert('User gone', usr + ' has left the chat');
-        }
-        socket.emit("get users");
-    });
-
-    socket.on('room change', function(usr, room){
-    	if(usr != gUser){
-    		$('#messages').append("<div class='clearfix'><li class='disconnect message' style='color:green'>The user <b>" + usr + "</b> has enter to room</li></div>");
-    		scrollDown();
-    	}else{
-    		$('#messages').append("<div class='clearfix'><li class='disconnect message' style='color:green'>Hello! Now you are on " + room + "</li></div>");
-    		$('#room').text( room + " chat room");
-    		scrollDown();
-    	}
-    });
-    socket.on('update users', function(list){
-        console.log("Actualizacion de usuarios", list);
-        usuarios = list;
-    });
-
-    socket.on('update roomies', function(roomies) {
-        if( typeof(roomies) !== "undefined" && roomies !== null ){
-            console.log("Actualizacion de roomies", roomies);
-            var lis ="";  roomies.forEach( function(u){ 
-                lis += "<li class='mdl-navigation__link'>" + u + "</li>"; 
-            }); $('#users').html(lis);
+    // Send messages and photos triggers
+    $send.on("keypress", function(e){
+        if(e.which == 13 && !e.shiftKey){
+            e.preventDefault();
+            sendMessage();
         }
     })
 
-    socket.on('retry-login', function(){
-    	setTimeout(login($('#clock').val(), $('#password').val()), 200)
-    });
+    // Change current room on click
+    $("tabs-chat", ".chat-window").on("click", function(){
+        $currentroom = $(this).attr("id").split("-").pop();
+    })
+});
 
-
-    $(document).ready(function(){
-        $('#joinRoom').on('click', function(){
-            toggle('#selectRoom');
-        });
-
-        $('.joinRoom').on("click", function(){
-            socket.emit("change-room", $(this).attr("for"));
-            toggle('#selectRoom');
-            setTimeout(function(){
-                $('#drawer').removeClass("is-visible");
-            }, 500);
-        });
-
-    	$('#login').on("click", function(){
-    		if( $('#clock').val() == "" ) { alert("Please fill the data"); }else{ login($('#clock').val(), $('#password').val()); }
-    		// if( $('#clock').val() == "" || $('#password').val() == ""){ alert("Please fill the data"); }else{ login($('#clock').val(), $('#password').val()); }
-    	});
-
-        $('#password').bind('keypress', function(e) {
-            if ((e.keyCode || e.which) == 13){
-                // if( $('#clock').val() == "" || $('#password').val() == ""){ alert("Please fill the data"); }else{ login($('#clock').val(), $('#password').val()); }
-                if( $('#clock').val() == "" ){ alert("Please fill the data"); }else{ login($('#clock').val(), $('#password').val()); }
-            }
-        });
-
-    	initialize();
-    	console.log("Hello seahorse!");
-
-    	//Chat Audio!!!
-		$('<audio id="audio"><source src="/audio/sound.ogg" type="audio/ogg"></audio>').appendTo("body");
-    });
-
-function scrollDown(){ d = (navigator.userAgent.toLowerCase().indexOf('firefox') !== -1) ? '#container' : '.mdl-layout__content'; $(d).animate({ scrollTop: $('#container').prop("scrollHeight") }, 700, "swing" );}
-function initialize(){
-	if(typeof usuarios === 'undefined'){
-		socket.emit("get users");
-		setTimeout(initialize, 200);
-	}else{
-		$('#loading').fadeOut('fast');
-	}
+function notificate(title, message) {
+    if (Notification.permission !== "granted") {
+        Notification.requestPermission();
+    }
+    
+    if ($('#options-showAlerts').is(':checked')) {
+        var extra = {
+            body: getPlainText(message)
+        };
+        var noti = new Notification(title, extra);
+        noti.onclick = function () {
+            noti.close();
+        };
+        setTimeout(function () {
+            noti.close();
+        }, 5000);
+    }
+    
+    playAudio();
 }
 
-function login(username, password){
-    var response = {
-        loged : true,
-        name : username,
-        area : "FREE"
-    };
-    
-    $('.login-failed').remove();
-
-    var alreadyloged;
-    if(response.loged == true && response.name != null ){
-        for (key in usuarios) { if(response.name == usuarios[key]){ alreadyloged = true; } }
-        if(!alreadyloged){
-            socket.emit('new user', response.name, response.area);
-            res = response;
-            $('#loginfrm').slideUp('slow'); gUser = response.name;
-        }else{
-            var obj = new Object();
-            obj.username = username; obj.password = password; var json = JSON.stringify(obj);
-            socket.emit("force-disconnect", json );
-            console.log(json);
-            $('.form-login').prepend('<div class="login-failed">User already loged! Unloggin it, please wait</div>');
-        }
-    }else{
-        $('.form-login').prepend('<div class="login-failed">Password failed!</div>');
+function playAudio() {
+    if ($('#options-playSound').is(':checked')) {
+        $('#audio')[0].play();
     }
 }
 
-//window.location.assign("unauthorized");
-function showSmartAlert(title, message){ 
-	if($('#showAlerts').is(':checked')){ 
-		if (Notification.permission!=="granted"){ 
-			Notification.requestPermission(); 
-		} 
-		var extra = { body: message }; var noti= new Notification(title,extra);
-		noti.onclick = function() { 
-			noti.close();
-		}; 
-		setTimeout(function(){ noti.close(); }, 5000); 
-	} 
+function getPlainText(html) {
+    return $('<p>' + html + '</p>').text();
 }
 
-function playAudio(){
-	if($('#playSound').is(':checked')){ $('#audio')[0].play(); }
+function sendFile(inp) {
+    if (inp.files && inp.files[0]) {
+        var FR = new FileReader();
+        FR.fileName = inp.files[0].name
+        FR.onload = function (e) {
+            socket.emit('chat image', e.target.result);
+            $('.upload-form').append('<div class="success">The file ' + e.target.fileName + ' was sent to everybody</div>');
+            console.log(e.target);
+            setTimeout(function () {
+                toggle('#upload-form');
+                $('.success').remove();
+            }, 1500);
+        };
+        FR.readAsDataURL(inp.files[0]);
+    }
 }
+
+$('#send-image').on("click", function () {
+    sendFile(document.getElementById('input-image'));
+});
+
+/*document.getElementById("input-image-text").onchange = function () {
+    document.getElementById("input-image").value = this.files[0].name;
+};
+
+$('form').submit(function () {
+    if ($('#m').val() != '') {
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+    }
+    return false;
+});
+*/
